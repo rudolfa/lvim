afeed029a28da2e6bdbd2708e8f78c4ea7428773
andy <andreas.rudolf@gmx.com>
2023-05-13 14:42:10 +0200

    First common configurations
    - Personal preferences

---
+++ lvim/config.lua
@@ -0,0 +1,23 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+-- vim options
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.relativenumber = true
+
+-- general
+lvim.log.level = "info"
+lvim.format_on_save = {
+  enabled = true,
+  pattern = "*.lua",
+  timeout = 1000,
+}
+
+-- -- Change theme settings
+lvim.colorscheme = "desert"
+
+
+-- Automatically install missing parsers when entering buffer
+lvim.builtin.treesitter.auto_install = true
